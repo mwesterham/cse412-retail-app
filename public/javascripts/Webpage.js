2c2887a31a3dd63a0eca24aafce21024926a2312
Fabiansu06 <fasanch3@asu.edu>
2022-11-30 16:31:29 -0700

    Webpage button changes

---
+++ public/javascripts/Webpage.js
@@ -0,0 +1,23 @@
+$(function() {
+    console.log( "ready!" );
+    $( "#Login" ).click(function() {
+        Login();
+    });
+});
+
+function Login(){
+    //admin = supplier send to add list 
+    //otherwise send to buyer page
+    var Username = $("#Username").val();
+    console.log(Username);
+    
+    if(Username == "admin")
+    {
+        window.location.href = "/AddProduct.html";
+    }
+    else{
+        window.location.href = "/BuyerView.html";
+    }
+    
+
+}
