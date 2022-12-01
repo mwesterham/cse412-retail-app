3b6c1056345546fd35107bd1bd64d17efbb0b71b
Fabiansu06 <fasanch3@asu.edu>
2022-12-01 00:57:14 -0700

    Update UserInfo
    -Checkers added for createAccount

--- public/javascripts/userInfo.js
+++ public/javascripts/userInfo.js
@@ -1,9 +1,36 @@
   //Activated when a User clicks on the "Create Account" button
 
+
   $(function() {
     console.log( "ready!" );
     $( "#CreateAcct" ).click(function() {
+      //Variable initizations
+      var name = document.getElementById("Name").value;
+      var phoneNumber = parseInt(document.getElementById("PhoneNumber").value);
+      var address = document.getElementById("Address").value;
+      var email = document.getElementById("Email").value;
+      
+      if (name == "") {
+        alert("Please fill out your name");
+      }
+      else if (phoneNumber < 999999999) {
+        alert("Please input a phone number");
+      }
+      else if(phoneNumber === NaN)
+      {
+        alert("Please input a phone number");
+      }
+      else if (address == "") {
+        alert("Please fill out your address");
+      }
+      else if (email == "") {
+        alert("Please fill out your email");
+      }
+
+      else{
         NewButton();
+      }
+
     });
 });
 
