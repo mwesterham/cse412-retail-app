1be7b0fee2252350631198f45d79154262526ab0
Fabiansu06 <fasanch3@asu.edu>
2022-12-01 00:21:05 -0700

    Cart and AddProducts edits
    -Shopping Cart Nav bar edited
    -Has buyer info but still needs to be formatted properly
    -Add product now has checker for blanks

--- public/javascripts/AddProduct.js
+++ public/javascripts/AddProduct.js
@@ -1,16 +1,38 @@
 //Activated when a User clicks on the "Add Item" button
 // Shorthand for $( document ).ready()
+
+
 // On page load
-$(function() {
-    console.log( "ready!" );
-    $( "#AddItem" ).click(function() {
-        ItemAdded();
-    });
+$(function () {
+  console.log("ready!");
+  $("#AddItem").click(function () {
+    //Checker for any blank content
+    var pName = document.getElementById("ProductName").value;
+    var pSupply = document.getElementById("ProductSupply").value;
+    var pBrand = document.getElementById("ProductBrand").value;
+    var pPrice = document.getElementById("ProductPrice").value;
+    if (pName == "") {
+      alert("Please fill out the product name");
+    }
+    else if (pSupply <= 0) {
+      alert("Please input a digit greater than 0 for supply");
+    }
+    else if (pBrand == "") {
+      alert("Please fill out the product brand");
+    }
+    else if (pPrice <= 0) {
+      alert("Please input a digit greater than 0 for price");
+    }
+
+
+    else {
+      ItemAdded();
+    }
+  });
 });
 
 //Creates the "Back To Webpage" Button for the supplier to exit and lets them know their item has been added.   
-function ItemAdded() 
-{
-  document.getElementById("Notification").style.display='inline';//reveals the text for Notification
+function ItemAdded() {
+  document.getElementById("Notification").style.display = 'inline';//reveals the text for Notification
   document.getElementById("Notification").innerHTML = "Item has been Added!"//Modifies the information for Notification
 }
--- public/javascripts/shoppingCart.js
+++ public/javascripts/shoppingCart.js
@@ -16,6 +16,7 @@ $(function () {
     });*/
 
     getShoppingCart();
+    getBuyerInfo();
 });
 
 function getShoppingCart(id = 289) {
@@ -31,9 +32,54 @@ function getShoppingCart(id = 289) {
                 var this_listing = data[key];
                 console.log(this_listing);
                 var roundPrice = Math.round(this_listing.product_pricing * 100) / 100;
-                var row = $(`<tr><th>${this_listing ["product_name"]}</th><td>${this_listing ["brand"]}</td><td>${this_listing ["delivery_time"]}</td><td>${roundPrice}</td><td>${this_listing ["status"]}</td></tr>`);
+                var row = $(`<tr><th>${this_listing["product_name"]}</th><td>${this_listing["brand"]}</td><td>${this_listing["delivery_time"]}</td><td>${roundPrice}</td><td>${this_listing["status"]}</td></tr>`);
                 $("#tablebody").append(row);
             }
         });
 }
 
+/*
+function purchase() {
+
+
+
+}
+
+}*/
+
+function getBuyerInfo(id = 289) {
+    console.log("BuyerINFO");
+    axios.get('/get_buyer_info', {
+        params: {
+            buyer_id: id,
+        }
+    })
+    .then(function (response) {
+            var total = 32;
+            var table = document.getElementById("tablebody");
+            for (var i = 0, row; row = table.rows[i]; i++) {
+                //iterate through rows
+                //rows would be accessed using the "row" variable assigned in the for loop
+                for (var j = 0, col; col = row.cells[j]; j++) {
+                    //iterate through columns
+                    //columns would be accessed using the "col" variable assigned in the for loop
+                    if (j == 3) {
+                        alert("First alert" + table.row[i].col[j]);
+                        alert("Secondalert" + table.row[i].col[j].innerhtml);
+                        //total += table.row[i].col[j];
+                    }
+                }
+            }
+
+/*
+                // handle success
+                var data = response.data;
+                console.log(data);
+                //from buyer view needs to be modified for contact information
+                for (const key in data) {
+                    var contactInfo = data[key];
+                    console.log(contactInfo);
+                    var row = $(`<tr><td>${contactInfo["fullname"]}</td><td>${contactInfo["phone_number"]}</td><td>${contactInfo["address"]}</td><td>${contactInfo["email"]}</td><td>${total}</td><td>purchase</td></tr>`);
+                }*/
+            })
+}
