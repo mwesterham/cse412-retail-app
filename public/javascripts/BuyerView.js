b9df45c8f5f120e01ce37ab6fb87eb2c52df4e3d
Fabiansu06 <fasanch3@asu.edu>
2022-12-03 22:29:51 -0700

    Forgot to send with previous
    -buyer also has individual id

--- public/javascripts/BuyerView.js
+++ public/javascripts/BuyerView.js
@@ -1,24 +1,17 @@
-
+var loggedId = localStorage.getItem('UsersInfo');
 $(function () {
     
     console.log("ready!");
-    //creates a variable to obtain all the buttons with the same class btn-class
-    let btns = document.querySelectorAll(".btn-class");
-    //Allows all buttons with the same class to have the same event be handled
-    btns.forEach(btn => {
-        //When they are clicked the following event will be handled
-        btn.addEventListener('click', (event) => {
-            AddToCartButton();
-        });
+   // var gettingName = getName();
 
-    });
+    //console.log("getNames: " +gettingName);
     getListings();
 });
 
-function AddToCartButton(product_name, listing_id) {
+function AddToCartButton(loggedId, product_name, listing_id) {
     axios.get('/add_to_cart', {
         params: {
-            buyer_id: 289,
+            buyer_id: loggedId,
             listing_id: listing_id,
             quantity: 3,
             status: "In Cart"
@@ -37,15 +30,19 @@ function getListings() {
             // handle success
             var data = response.data.rows;
             console.log(data);
+            console.log("Id is" + loggedId);
             for (const key in data) {
                 var this_listing = data[key];
                 console.log(this_listing);
                 var roundPrice = Math.round(this_listing.product_pricing * 100) / 100;
-                var AddToCart = `<button type="button" onclick="AddToCartButton('${this_listing.product_name}', '${this_listing.listing_id}')" class="btn btn-success">Add To Cart</button>`;
+                var AddToCart = `<button type="button" onclick="AddToCartButton('${loggedId},${this_listing.product_name}', '${this_listing.listing_id}')" class="btn btn-success">Add To Cart</button>`;
                 var row = $(`<tr><td>${this_listing["product_name"]}</td><td>${this_listing["brand"]}</td><td>${roundPrice}</td><td>${this_listing["product_supply"]}</td><td>${AddToCart}</td></tr>`);
-                $("#tablebody").append(row);
+                $("#buyerTableBody").append(row);
+              //  $("#cartTableBody").append(row);    
             }
+          //  localStorage.setItem("cartTableBody");
         });
 }
+
 
 
